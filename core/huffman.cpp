b854029e55b3a61bd68493636279d1d3f5c68142
Kerem <20242013047@std.nisantasi.edu.tr>
2026-03-03 20:56:41 +0300

    huffman algorithms completed.

--- core/huffman.cpp
+++ core/huffman.cpp
@@ -3,3 +3,48 @@
 //
 
 #include "huffman.h"
+#include <iostream>
+using namespace std;
+
+huffman::node::node(char ch, int freq) : ch(ch), freq(freq), left(nullptr), right(nullptr) {}
+huffman::node::node(char ch, int freq, node* left, node* right) : ch(ch), freq(freq), left(left), right(right) {}
+
+bool huffman::compare::operator()(node* left, node* right) {
+    return left->freq > right->freq;
+}
+
+void huffman::createHuffmanTree(const string& text) {
+    for (char c : text) {
+        frequancies[c]++;
+    }
+
+    priority_queue<node*, vector<node*>, compare> pq;
+    for (auto& [ch, freq] : frequancies) {
+        pq.push(new node(ch, freq));
+    }
+    while (pq.size() > 1) {
+        node* left = pq.top();
+        pq.pop();
+        node* right = pq.top();
+        pq.pop();
+        pq.push(new node('\0', left->freq + right->freq, left, right));
+    }
+    root = pq.top();
+
+    printHuffmanCode(root, "");
+
+    for (auto pair : huffmanCode) {
+        cout << pair.first << " " << pair.second << endl;
+    }
+
+}
+
+void huffman::printHuffmanCode(node* node, string code) {
+    if (node == nullptr){return;}
+    if (node->left == nullptr && node->right == nullptr) {
+        huffmanCode[node->ch] = code;
+        return;
+    }
+    printHuffmanCode(node->left, code + "0");
+    printHuffmanCode(node->right, code + "1");
+}
--- core/huffman.h
+++ core/huffman.h
@@ -5,6 +5,36 @@
 #ifndef ZIPBIKE_HUFFMAN_H
 #define ZIPBIKE_HUFFMAN_H
 
-class huffman {};
+#include <string>
+#include <map>
+#include <queue>
+
+class huffman {
+
+    struct node {
+        char ch;
+        int freq;
+        node *left;
+        node *right;
+
+        node(char c, int freq);
+        node(char ch, int freq, node* left, node* right);
+    };
+
+    struct compare {
+        bool operator()(node* a, node* b);
+    };
+
+    std::map<char, int> frequancies;
+    std::map<char, std::string> huffmanCode;
+    node* root;
+
+    void printHuffmanCode(node* node, std::string code);
+    void createHuffmanFreq(std::string& text);
+
+    public:
+
+    void createHuffmanTree(const std::string& text);
+};
 
 #endif // ZIPBIKE_HUFFMAN_H
