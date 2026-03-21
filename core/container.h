6dfd9beae8f8d2e7e7d8697d9172fbc8400a0694
Iliya Dindar <157999379+iliyadindar@users.noreply.github.com>
2026-03-22 02:03:22 +0300

    Update container.h

--- core/container.h
+++ core/container.h
@@ -5,6 +5,42 @@
 #ifndef ZIPBIKE_CONTAINER_H
 #define ZIPBIKE_CONTAINER_H
 
-class container {};
+#include <vector>
+#include <cstdint>
+#include <string>
+
+enum CompressionType {
+    NONE,
+    RLE,
+    LZ77,
+    HUFFMAN
+};
+
+class container {
+public:
+
+    container();
+
+    container(CompressionType type, std::vector<uint8_t> compressedData, int originalSize);
+
+    void setData(std::vector<uint8_t> compressedData);
+    void setType(CompressionType type);
+    void setOriginalSize(int size);
+
+    std::vector<uint8_t> getData();
+    CompressionType getType();
+    int getOriginalSize();
+    int getCompressedSize();
+
+    bool isEmpty();
+
+    void clear();
+
+private:
+
+    std::vector<uint8_t> data;
+    CompressionType type;
+    int originalSize;
+};
 
 #endif // ZIPBIKE_CONTAINER_H
