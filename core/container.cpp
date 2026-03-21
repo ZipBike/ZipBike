3cf913d0690b398cb79f248aae9080465d858968
Iliya Dindar <157999379+iliyadindar@users.noreply.github.com>
2026-03-22 02:04:53 +0300

    Update container.cpp

--- core/container.cpp
+++ core/container.cpp
@@ -3,3 +3,56 @@
 //
 
 #include "container.h"
+
+container::container() {
+    type = NONE;
+    originalSize = 0;
+}
+
+container::container(CompressionType type, std::vector<uint8_t> compressedData, int originalSize) {
+    this->type = type;
+    this->data = compressedData;
+    this->originalSize = originalSize;
+}
+
+// --- Setters ---
+
+void container::setData(std::vector<uint8_t> compressedData) {
+    data = compressedData;
+}
+
+void container::setType(CompressionType type) {
+    this->type = type;
+}
+
+void container::setOriginalSize(int size) {
+    originalSize = size;
+}
+
+// --- Getters ---
+
+std::vector<uint8_t> container::getData() {
+    return data;
+}
+
+CompressionType container::getType() {
+    return type;
+}
+
+int container::getOriginalSize() {
+    return originalSize;
+}
+
+int container::getCompressedSize() {
+    return data.size();
+}
+
+bool container::isEmpty() {
+    return data.empty();
+}
+
+void container::clear() {
+    data.clear();
+    type = NONE;
+    originalSize = 0;
+}
