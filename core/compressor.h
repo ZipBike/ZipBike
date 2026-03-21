712b6ec66518f2a0515c4c2255e5b4c3e163218c
Iliya Dindar <157999379+iliyadindar@users.noreply.github.com>
2026-03-22 02:09:52 +0300

    Update compressor.h

--- core/compressor.h
+++ core/compressor.h
@@ -5,6 +5,30 @@
 #ifndef ZIPBIKE_COMPRESSOR_H
 #define ZIPBIKE_COMPRESSOR_H
 
-class compressor {};
+#include <vector>
+#include <cstdint>
+#include "container.h"
+
+class compressor {
+public:
+
+    compressor();
+
+    container compress(std::vector<uint8_t> inputData, CompressionType type);
+
+    std::vector<uint8_t> decompress(container& c);
+
+    void printStats(container& c);
+
+private:
+
+    std::vector<uint8_t> compressRLE(std::vector<uint8_t>& data);
+    std::vector<uint8_t> compressLZ77(std::vector<uint8_t>& data);
+    std::vector<uint8_t> compressHuffman(std::vector<uint8_t>& data);
+
+    std::vector<uint8_t> decompressRLE(std::vector<uint8_t>& data);
+    std::vector<uint8_t> decompressLZ77(std::vector<uint8_t>& data);
+    std::vector<uint8_t> decompressHuffman(std::vector<uint8_t>& data);
+};
 
 #endif // ZIPBIKE_COMPRESSOR_H
