05f6d6cd176519019a54f770329e1bc48258afbd
Iliya Dindar <157999379+iliyadindar@users.noreply.github.com>
2026-03-22 01:42:08 +0300

    Update bitstream.h

--- core/bitstream.h
+++ core/bitstream.h
@@ -5,6 +5,24 @@
 #ifndef ZIPBIKE_BITSTREAM_H
 #define ZIPBIKE_BITSTREAM_H
 
-class bitstream {};
+#include <vector>
+#include <cstdint>  // for uint8_t
+
+class bitstream {
+public:
+    bitstream();
+    void writeBit(int bit);
+    void writeByte(uint8_t byte);
+    int readBit();
+    bool isEmpty();
+    int size();
+    void clear();
+
+private:
+
+    std::vector<uint8_t> data;
+    int totalBits;
+    int readPosition;
+};
 
 #endif // ZIPBIKE_BITSTREAM_H
