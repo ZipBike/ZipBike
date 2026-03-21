57493a6872047650ca75352f0d7ca935a003e5c0
Iliya Dindar <157999379+iliyadindar@users.noreply.github.com>
2026-03-22 01:49:20 +0300

    Update bitstream.cpp

--- core/bitstream.cpp
+++ core/bitstream.cpp
@@ -3,3 +3,59 @@
 //
 
 #include "bitstream.h"
+
+bitstream::bitstream() {
+    totalBits = 0;
+    readPosition = 0;
+}
+
+void bitstream::writeBit(int bit) {
+
+    if (totalBits % 8 == 0) {
+        data.push_back(0);
+    }
+
+    if (bit == 1) {
+        int byteIndex = totalBits / 8;
+        int bitIndex  = 7 - (totalBits % 8);
+
+        data[byteIndex] |= (1 << bitIndex);
+    }
+
+    totalBits++;
+}
+
+void bitstream::writeByte(uint8_t byte) {
+    for (int i = 7; i >= 0; i--) {
+        int bit = (byte >> i) & 1;
+        writeBit(bit);
+    }
+}
+
+int bitstream::readBit() {
+
+    if (readPosition >= totalBits) {
+        return -1;
+    }
+
+    int byteIndex = readPosition / 8;
+    int bitIndex  = 7 - (readPosition % 8);
+
+    readPosition++;
+
+    return (data[byteIndex] >> bitIndex) & 1;
+}
+
+bool bitstream::isEmpty() {
+    return readPosition >= totalBits;
+}
+
+int bitstream::size() {
+    return totalBits;
+}
+
+void bitstream::clear() {
+    data.clear();
+    totalBits = 0;
+    readPosition = 0;
+}
